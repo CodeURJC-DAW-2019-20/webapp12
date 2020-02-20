64e255639243fe49da4118a50456780bcd2ba492
Maria Flores Garcia <mariafloresgarcia@gmail.com>
2020-02-20 12:13:41 +0100

    Property-single updated
    pick information from db and resize the images

--- daw.webapp12/src/main/java/com/daw/webapp12/controller/AdvertisementController.java
+++ daw.webapp12/src/main/java/com/daw/webapp12/controller/AdvertisementController.java
@@ -13,6 +13,7 @@ import com.daw.webapp12.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -85,5 +86,10 @@ public class AdvertisementController {
         return "index";
 	}
 
+	@RequestMapping(value = "/properties/{id}")
+    public String favAdvertisements(Model model, @PathVariable  long id) {
+	 	model.addAttribute("Property", advertisementService.findById(id));
+        return "properties-single";
+    }
 	
 }
