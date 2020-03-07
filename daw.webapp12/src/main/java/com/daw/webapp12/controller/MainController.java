7b0644aa1bd626ee684d9647149fed9bea2ccf7d
Maria Flores Garcia <mariafloresgarcia@gmail.com>
2020-03-07 09:18:13 +0100

    Docker added

--- daw.webapp12/src/main/java/com/daw/webapp12/controller/MainController.java
+++ daw.webapp12/src/main/java/com/daw/webapp12/controller/MainController.java
@@ -1,5 +1,12 @@
 package com.daw.webapp12.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.daw.webapp12.entity.Advertisement;
 import com.daw.webapp12.entity.Users;
 import com.daw.webapp12.repository.AdvertisementRepository;
@@ -11,19 +18,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 
 @Controller
--- daw.webapp12/src/main/java/com/daw/webapp12/controller/UserController.java
+++ daw.webapp12/src/main/java/com/daw/webapp12/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.daw.webapp12.controller;
 
-import java.util.Arrays;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,10 +9,8 @@ import com.daw.webapp12.security.UserComponent;
 import com.daw.webapp12.security.UserRepositoryAuthenticationProvider;
 import com.daw.webapp12.service.AdvertisementService;
 import com.daw.webapp12.service.UserService;
-import com.daw.webapp12.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,8 +19,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 
 
