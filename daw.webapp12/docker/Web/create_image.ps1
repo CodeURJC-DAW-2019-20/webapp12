cd7bf4734602914f7ae5073d58823d2ec3d08142
Maria Flores Garcia <mariafloresgarcia@gmail.com>
2020-03-09 19:20:24 +0100

    Create image fixed
    script to create new jar updated

--- daw.webapp12/docker/Web/create_image.ps1
+++
@@ -1,5 +0,0 @@
-cd ./daw.webapp12
-docker run -imysqlt --rm --name mavenInstalation -v ${PWD}:/src -w /src maven:3.6-jdk-8 mvn clean install
-mvn package
-Copy-Item './target/daw.webapp12-0.0.1-SNAPSHOT.jar' '../webapp12.jar'
-Write-Output 'Compilacion completa completada'
