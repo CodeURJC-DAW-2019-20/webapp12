a295aaef64e7cc9d938f21a1afd6fa97f28ff09e
alilu8 <32311130+alilu8@users.noreply.github.com>
2020-04-12 00:21:21 +0200

    Update app.module.ts
    add ReactiveFormsModule

--- WebApp12Angular/src/app/app.module.ts
+++ WebApp12Angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MatIconRegistry } from '@angular/material/icon';
 
@@ -74,6 +74,7 @@ import { blogSingleComponent } from './blog-single.component';
         BrowserModule,
         BrowserAnimationsModule,
         FormsModule,
+        ReactiveFormsModule,
         RouterModule.forRoot([]),
         HttpClientModule,
         /** Material Modules */
