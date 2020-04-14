108a00ec6d64d088f4475dd07ac393e91e6e1150
Eduardo Luna <EduLuna@users.noreply.github.com>
2020-04-14 21:17:15 +0200

    Update app.routing.ts

--- WebApp12Angular/src/app/app.routing.ts
+++ WebApp12Angular/src/app/app.routing.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { FavoritosComponent } from './pages/favoritos/favoritos.component';
 import { blogSingleComponent } from './blog-single.component';
+import { AuthUserService } from './service/auth/auth.user.service';
 import { PropertyUploadComponent } from './property-upload/propertyUpload.component';
 //import { PropertyUploadComponent } from './property-upload/propertyUpload.component';
 
@@ -19,7 +20,7 @@ const appRoutes = [
   { path: 'advertisement', component: AdvertisementComponent},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-  { path: 'favoritos/:id', component: FavoritosComponent },
+  { path: 'favoritos/:id', component: FavoritosComponent , canActivate: [AuthUserService]},
   { path: 'book/new', component: BookFormComponent },
   { path: 'book/:id', component: BookDetailComponent },
   { path: 'book/edit/:id', component: BookFormComponent },
