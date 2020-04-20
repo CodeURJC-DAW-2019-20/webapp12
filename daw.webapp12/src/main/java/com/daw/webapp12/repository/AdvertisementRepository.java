f573c01a81457aee893192dfb9ffebbc97133173
Maria Flores Garcia <mariafloresgarcia@gmail.com>
2020-04-20 10:44:32 +0200

    query bug fixed

--- daw.webapp12/src/main/java/com/daw/webapp12/repository/AdvertisementRepository.java
+++ daw.webapp12/src/main/java/com/daw/webapp12/repository/AdvertisementRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface AdvertisementRepository extends JpaRepository<Advertisement, Long> {
     List<Advertisement> findByLocation(String string);
 
-    @Query(value = "SELECT * FROM Advertisement WHERE square_meters >= ?1 AND type = ?2 AND bathrooms >= ?3 AND location = ?4 AND price <=?5 AND rooms >= ?6 LIMIT ?7",nativeQuery = true)
+    @Query(value = "SELECT * FROM advertisement WHERE square_meters >= ?1 AND type = ?2 AND bathrooms >= ?3 AND location = ?4 AND price <=?5 AND rooms >= ?6 LIMIT ?7",nativeQuery = true)
     List<Advertisement> findPreferences(int square_meters, String type, int bathrooms, String location, int price, int rooms, int limit);
 
     
